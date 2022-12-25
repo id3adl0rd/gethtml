384eaa3729078606ce907216e7643ee319c24b0b
callbigpapa <raindeadobzor2@gmail.com>
2022-12-25 14:36:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,37 +6,43 @@ namespace html
 {
     class Program
     {
-        public static String GetCode(string urlAddress)
-        {
-            string data = "";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = null;
-                if (response.CharacterSet == null)
-                {
-                    readStream = new StreamReader(receiveStream);
-                }
-                else
-                {
-                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-                }
-                data = readStream.ReadToEnd();
-                response.Close();
-                readStream.Close();
-            }
-            return data;
-        }
-
         static void Main()
         {
-            string text = GetCode("https://lfwb.ru/index.php?title=Main_Page");
-            Console.WriteLine(text);
-            string path = "web-page.html";
-            File.WriteAllText(path, text);
-            Console.ReadKey();
+            WebRequest webr = WebRequest.Create("https://lfwb.ru/index.php?title=Main_Page");
+            HttpWebResponse resp = null;
+
+            try
+            {
+                resp = (HttpWebResponse)webr.GetResponse();
+            }
+            catch (WebException e)
+            {
+                resp = (HttpWebResponse)e.Response;
+            }
+
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream, Encoding.GetEncoding(resp.CharacterSet));
+
+            string sReadData = sr.ReadToEnd();
+
+            switch (resp.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    Console.WriteLine("Соединение имеется");
+                    break;
+                case HttpStatusCode.Forbidden:
+                    Console.WriteLine("Доступ запрещен");
+                    break;
+                case HttpStatusCode.NotFound:
+                    Console.WriteLine("Документ не найден");
+                    break;
+                case HttpStatusCode.Moved:
+                    Console.WriteLine("Документ перемещен");
+                    break;
+                default:
+                    Console.WriteLine("Что-то пошло не так");
+                    break;
+            }
         }
     }
 }
